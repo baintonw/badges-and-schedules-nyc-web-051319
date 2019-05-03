773e5a8f72519556a696c415f439a3369920cea6
William Bainton <baintonw@gmail.com>
2019-05-03 17:04:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,10 +9,12 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(array)
- room_counter = 1
+ room_counter = 0
   array.collect do |x|
-    "Hello, #{x}! You will be assigned to room !"
-    
+    room_counter += 1
+    "Hello, #{x}! You will be assigned to room #{room_counter}!"
+    if room_counter = 7
+    break
   end
 end
 
