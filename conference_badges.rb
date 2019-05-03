fd3f60bf064ae89f3cc797c5dc4b31fa4809bd41
William Bainton <baintonw@gmail.com>
2019-05-03 16:54:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,7 +8,7 @@ def batch_badge_creator(array)
   end
 end
 
-def assign_rooms
+def assign_rooms(array)
   room_counter = 1
   while room_counter <= 7
     "Hello, NAME! You'll be assigned to room #{room_counter}"
