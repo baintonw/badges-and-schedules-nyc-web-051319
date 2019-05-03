558a6ec9a353aa48d456c152eadd46359f06cd1c
William Bainton <baintonw@gmail.com>
2019-05-03 16:31:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def badge_maker(name)
+  ("Hello, my name is #{name}.")
+end
+
+def batch_badge_creator(array)
+  array.collect do |name|
+    "Hello, my name is #{name}."
+  end
+end
+
+def assign_room
+  
+end
